0bf81617b49601021360f8ddc90e077e766603b1
Joby Joseph <reachjoby@gmail.com>
2019-08-11 22:03:02 +0530

    onclick of textview

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener { updateNickname(it) }
     }
 
     private fun addNickname(view: View) {
@@ -30,5 +31,18 @@ class MainActivity : AppCompatActivity() {
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit);
+        val doneButton = findViewById<Button>(R.id.done_button);
+        editText.visibility = View.VISIBLE;
+        doneButton.visibility = View.VISIBLE;
+        view.visibility = View.GONE;
+
+        editText.requestFocus();
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
